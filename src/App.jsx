6c141b1fdbee44400094d2ac12cb891c464ce5ea
R&D Team <teams@ornatesolar.in>
2024-05-27 17:55:58 +0530

    tanstack query done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { AppContext } from './context/NewContext';
 import Navbar from './components/Navbar'
 import { decrement, increment } from './redux/counter/counterSlice'
 import { useSelector, useDispatch } from 'react-redux'
+import { useQuery } from '@tanstack/react-query';
 
 
 
@@ -14,7 +15,28 @@ function App() {
   const {variable,setVariable} = useContext(AppContext)//this is how you access context variables 
   const sliceCount = useSelector((state) => state.counter.value)//this is how you access a react slice variable(so the reducer function and the useSelector functions the first variables passed is the redux state)//The state variable passed to the useSelector hook represents the current state of your Redux store. When you use useSelector, React Redux subscribes your component to the Redux store. Whenever an action is dispatched and the Redux store is updated, React Redux will re-run your selector function with the new state of the store. If the output of your selector function is different from the previous result, React Redux will re-render your component.
   const dispatch = useDispatch()
+  const { data, error, isLoading } = useQuery({
+    queryKey:['JFF'],
+    queryFn: async () => {
+      const response = await fetch('http://192.168.1.124:8000/api/auth/signin/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: 'yash.ornatesolar@gmail.com',
+          password: '12345678',
+        }),
+      });
 
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    },
+  })
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
   return (
     <>
       <div>
@@ -26,7 +48,7 @@ function App() {
         <button onClick={()=>dispatch(increment(5))}>+The slice count is {sliceCount}</button>
         <button onClick={()=>dispatch(decrement())}>-The slice count is {sliceCount}</button>
       </div>
-     
+     <div>{JSON.stringify(data)}</div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,18 @@ import './index.css'
 import { AppProvider } from './context/NewContext.jsx'//context provider
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux' //provider that willprovide the store to whole of the app
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </Provider>
-    
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
